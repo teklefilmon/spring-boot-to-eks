8b219b5e71133d0de82f4183470ab6e5628e8a25
Filmon G Tekle <teklefilmon@gmail.com>
2023-10-11 19:56:03 -0500

    initial commit

---
+++ src/main/java/com/eritours/SpringBootToEksApplication.java
@@ -0,0 +1,13 @@
+package com.eritours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToEksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootToEksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eritours/SpringBootToEksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eritours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToEksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
